8820e694cc2fea2313d8c9028ff190d1940e7d00
Rares Andrei <36548850+rsh1337@users.noreply.github.com>
2022-03-15 03:03:40 +0200

    fix

--- pages/search.js
+++ pages/search.js
@@ -8,77 +8,12 @@ import {
   Box,
   Grid,
   GridItem,
-  Stack,
-  HStack,
-  Heading,
+
 } from "@chakra-ui/layout";
-import { CircularProgress, Progress  } from "@chakra-ui/progress";
-import Image from "next/image";
+import { Progress  } from "@chakra-ui/progress";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import Layout from "../components/layout";
-import { Center } from "@chakra-ui/layout";
-import Head from "next/head";
-import { buildImageUrl } from '../utils/api';
-import { Tag } from "@chakra-ui/tag";
-
-const MovieContent = () => {
-  const { id } = useRouter().query;
-  const { data, error } = useSWR(id && `/api/{id}?`);
-
-  if (error) {
-    return (
-      <Text color="red">
-        Error fetching movie with ID {id}: {JSON.stringify(error)}
-      </Text>
-    );
-  }
-  if (!data) {
-    return (
-      <Center h="full">
-        <CircularProgress isIndeterminate />
-      </Center>
-    );
-  }
-  return (
-    <Stack direction={['column', 'row']} spacing={4}>
-      <Head>
-        <title>{data.title}</title>
-      </Head>
-      <Box minW="300px" pos="relative">
-        <Image
-          src={buildImageUrl(data.poster_path, 'w300')}
-          alt="Movie poster"
-          layout="responsive"
-          width="300"
-          height="450"
-          objectFit="contain"
-          unoptimized
-        />
-      </Box>
-      <Stack>
-        <HStack justify="space-between">
-          <Heading as="h2">{data.title}</Heading>
-          <Box>
-            <Tag colorScheme="purple" variant="solid">
-              {data.release_date}
-            </Tag>
-          </Box>
-        </HStack>
-        <Box>{data.tagline}</Box>
-
-        <Stack direction="row">
-          {data.genres?.map((genre) => (
-            <Badge key={genre.id} colorScheme="purple" variant="outline">
-              {genre.name}
-            </Badge>
-          ))}
-        </Stack>
-        <Box>{data.overview}</Box>
-      </Stack>
-    </Stack>
-  );
-};
 
 function SearchResults() {
   const { terms } = useRouter().query;
@@ -112,7 +47,6 @@ function SearchResults() {
           align="center"
         >
           <Box minW="300px" pos="relative">
-          <MovieContent />
             <Link href={`/movies/${id}`} passHref>
               <Box height="50">
                 <Button
