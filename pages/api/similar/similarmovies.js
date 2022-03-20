e2ece7a709d0f3de9cbb8dc763bef7c4a41a9e4a
Rares Andrei <36548850+rsh1337@users.noreply.github.com>
2022-03-20 23:44:23 +0200

    .

--- pages/api/similar/similarmovies.js
+++
@@ -1,8 +0,0 @@
-import dbConnect from "../../../utils/dbConnect";
-import Watchlist from "../../../models/Watchlist";
-
-export default async function handler(req, res) {
-  await dbConnect();
-  let similarmovies = await Watchlist.findOne({id});
-  res.status(200).json(similarmovies);
-}
